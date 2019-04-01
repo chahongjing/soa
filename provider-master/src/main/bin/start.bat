7ebe675003af2e01ce99a4f4d21cfa8d4969a09a
chahongjing <310510906@qq.com>
2019-04-01 11:46:16 +0800

    完善dubbo

---
+++ provider-master/src/main/bin/start.bat
@@ -0,0 +1,1 @@
+java -jar provider-1.0.jar > "provider.log" 2>&1
---
+++ provider-slave/src/main/java/com/zjy/App.java
@@ -0,0 +1,11 @@
+package com.zjy;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[]{"applicationProvider.xml", "classpath:applicationContext-*.xml"});
+//        com.alibaba.dubbo.container.Main.main(args);
+    }
+}
